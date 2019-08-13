5cfdbcf955ba4e3daee8b454bf6fdd31dc5ac97b
VictorCarmonaMoraza <VictorCarmona9035@hotmail.com>
2019-08-13 13:24:42 +0200

    Primer Programa Java
    Esta aplicacion de java imprime por consola Hola Mundo

---
+++ JAVAGITHUB/src/PrimeroPasos/HolaMundo.java
@@ -0,0 +1,14 @@
+package PrimeroPasos;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Imprime Hola Mundo
+		 */
+		System.out.println("Hola Mundo");
+
+	}
+
+}
